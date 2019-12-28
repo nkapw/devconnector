f4bc562339777b3f06a4a14f1c839aa34f987932
imamfahrezi <imamfahrezi24@gmail.com>
2019-12-28 15:35:28 +0700

    get & delete post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,8 @@ const auth = require('../../middleware/auth')
 const Post = require('../../models/Post')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
+
+
 // @route GET api/posts
 // @desc create a post
 // @access private
@@ -33,7 +35,66 @@ router.post('/', [ auth, [
         console.error(err.message);
         res.status(500).send('Server error')
     }
-
 })
+
+
+
+// @route GET api/posts
+// @desc get all posts
+// @access private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 })
+        res.json(posts)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+})
+
+
+
+// @route GET api/posts/:id
+// @desc get post by id
+// @access private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(404).json({ msg: 'post not found' })
+        }
+        res.json(post)
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind  === 'ObjectId') {
+            return res.status(404).json({ msg: 'post not found' })
+        }
+        res.status(500).send('Server error')
+    }
+})
+
+
+
+// @route DELETE api/posts/:id
+// @desc delete a posts
+// @access private
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(404).json({ msg: 'post not found' })
+        }
+        // check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'user not authorized' })
+        }
+        await post.remove()
+        res.json({msg: 'post removed'})
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+})
+
 
 module.exports = router;
