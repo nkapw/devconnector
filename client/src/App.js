e0980fdcc549bcb7f198056f9f812b9a9fb25210
imamfahrezi <imamfahrezi24@gmail.com>
2019-12-31 18:04:22 +0700

    React & Concurrently setup and Clean up $ initial Components

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import Navbar from "./components/Layout/Navbar";
+import Landing from "./components/Layout/Landing";
+import './App.css';
+
+const App = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <Landing /> 
+    </Fragment>
+  )
+}
+
+export default App;
