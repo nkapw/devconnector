769bdeea8d2984bc4ec976f0eacdcb9667ae77e5
imamfahrezi <imamfahrezi24@gmail.com>
2020-01-02 15:22:40 +0700

    Alert Component & Action Call

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,11 +2,12 @@ import { SET_ALERT, REMOVE_ALERT } from "../actions/types";
 
 const initialState = []
 
-export default (state = initialState, action) => {
-  const { type, payload}
+export default function (state = initialState, action) {
+  const { type, payload} = action;
+  
   switch (type) {
     case SET_ALERT:
-      return [...state, action.payload];
+      return [...state, payload];
     case REMOVE_ALERT:
       return state.filter(alert => alert.id !== payload);
     default:
