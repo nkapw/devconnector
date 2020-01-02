332ec569c027827906a82f6a68c119a36ae30f27
imamfahrezi <imamfahrezi24@gmail.com>
2020-01-02 20:29:45 +0700

    Load User & Set Auth Token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -20,7 +20,7 @@ router.get('/', auth, async (req, res) => {
     }
 })
 
-// @route GET api/auth
+// @route POST api/auth
 // @desc Authentication user & get token
 // @access public
 router.post('/', [
