820ea4cf40231d0d30cc556d224300c9d4bccf35
imamfahrezi <imamfahrezi24@gmail.com>
2020-01-08 20:53:06 +0700

    Delete Education, Experiences & Account

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const auth = require('../../middleware/auth')
 const { check, validationResult } = require('express-validator')
 const Profile =  require('../../models/Profile')
 const User =  require('../../models/User')
+const Post =  require('../../models/Post')
 
 // @route GET api/profile/me
 // @desc Get Currnet users profile
@@ -140,7 +141,8 @@ router.get('/user/:user_id', async (req, res) => {
 // @access Private
 router.delete('/', auth, async (req, res) => {
     try {
-        //@todo - remove users posts
+        // Remove User posts
+        await Post.deleteMany({ user: req.user.id })
         //remove profile
         await Profile.findOneAndRemove({ user: req.user.id })
         //remove user
