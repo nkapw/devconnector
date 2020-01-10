2d567df3532746f27e848776a7c420ebfa6b68e2
imamfahrezi <imamfahrezi24@gmail.com>
2020-01-10 16:30:20 +0700

    Post Item Component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -10,7 +11,16 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
   }, [getPosts])
   return loading ? <Spinner /> : (
     <Fragment>
-      test
+      <h1 className="large text-primary">posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome to the community
+      </p>
+      {/* PoatForm */}
+      <div className="posts">
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
     </Fragment>
   )
 };
